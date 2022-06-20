b0ccb3eff65a28338da30461bd3ac6b4a7a85380
Br-um <henriquedsdbrum@gmail.com>
2022-06-19 21:31:44 -0300

    Commit-5

---
+++ models/pedido.php
@@ -0,0 +1,41 @@
+<?php
+$nome = isset($_POST['nome']) ? $_POST['nome'] : "";
+$telefone = isset($_POST['telefone']) ? $_POST['telefone'] : "";
+$cep = isset($_POST['cep']) ? $_POST['cep'] : "";
+$produto = isset($_POST['produto']) ? $_POST['produto'] : "";
+$qnt = isset($_POST['quantidade']) ? $_POST['quantidade'] : "";
+$receber = isset($_POST['receber']) ? $_POST['receber'] : "";
+
+echo "<h2>Nome: " . $nome . "</h2>";
+echo "<h4>telefone: " . $telefone . "</h4>";
+echo "<h4>CEP: " . $cep . "</h4>";
+echo "<h4>Produto: " . $produto . "</h4>";
+echo "<h4>Quantidade: " . $qnt . "</h4>";
+
+if (isset($_POST['receber'])) {
+    echo "<h4>Modo de reber: " . $_POST['receber'] . "</h4>";
+} else {
+    echo "Você nao escolheu nenhum metodo de recebimento"
+}
+echo "<br><br><br><br><hr>";
+
+$valor;
+switch ($produto) {
+    case 'Pao':
+        $valor = 0.80;
+        break;
+    case 'Sabonete':
+        $valor = 5;
+        break;
+    case 'Costela de porco':
+        $valor = 30;
+        break;
+    default:
+        echo "<h3>Você não escolheu um valor válido</h3>";
+}
+
+
+$valorTotal = $valor * $qnt;
+
+echo "<h2>Valor Total: " . $valorTotal . "</h2>";
+echo "<h4>Valor Unitário: " . $valor . "</h4>";
